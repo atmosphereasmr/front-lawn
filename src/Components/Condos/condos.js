04ae665f5a4c58fe689e65971c50ae56fc37f9bc
Logan <atmosphereasmr@gmail.com>
2018-04-26 00:22:22 -0700

    okay

--- src/Components/Condos/condos.js
+++ src/Components/Condos/condos.js
@@ -19,7 +19,7 @@ export default class Condos extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://housebnb.herokuapp.com/api/home'
+        axios.get('http://housebnb.herokuapp.com:3001/api/home'
         )
             .then(res => {
                 const rawApartments = [res.data]
